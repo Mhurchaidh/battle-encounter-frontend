542081ff6239be4afef043b46fcab2196a4e7fa9
mhurchaidh <bmurchie1997@gmail.com>
2023-02-16 16:37:23 -0800

    Refactored components to meet MVP

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import Battleground from './components/Battleground';
 
 
 function App() {
-  const [encounter, setEncounter] = useState([])
-
-  useEffect(() => {
-    fetch('http://localhost:9292', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    })
-    .then(resp => resp.json())
-    .then(resp => setEncounter(resp))
-  }, [])
-
-  const handleEncounterChange = (obj) => {
-    setEncounter(obj)
-  }
-
   return (
     <div className="App">
-      <Battleground encounter={encounter} handleEncounterChange={handleEncounterChange}/>
+      <Battleground/>
     </div>
   );
 }
--- src/components/Battleground.js
+++ src/components/Battleground.js
@@ -1,13 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CharacterHolder from './CharacterHolder.js';
 import EnemyHolder from './EnemyHolder';
 
-function Battleground({encounter, handleEncounterChange}) {
+function Battleground() {
 
     const [initiator, setInitiator] = useState(0)
     const [target, setTarget] = useState(0)
     const [playerAttacking, setPlayerAttacking] = useState(false)
+    //const [encounter, setEncounter] = useState({})
+    const [battlegrounds, setBattlegrounds] = useState([])
+    const [characters, setCharacters] = useState([])
+    const [enemies, setEnemies] = useState([])
 
+    useEffect(() => {
+        fetch('http://localhost:9292', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            setBattlegrounds(resp.battlegrounds)
+            setCharacters(resp.characters)
+            setEnemies(resp.enemies)
+        })
+    }, [])
+
+    
     const onAttackSubmit = () => {
         const config = {
             method: "POST",
@@ -22,26 +42,33 @@ function Battleground({encounter, handleEncounterChange}) {
             }
             )
         }
-        fetch('http://localhost:9292', config)
-        .then(resp => resp.json())
-        .then((resp) => {handleEncounterChange(resp)})
-        .then(setPlayerAttacking(false))
-    }
-
-    const handleBattleReset = () => {
-        fetch('http://localhost:9292', {method: "DELETE"})
+        fetch('http://localhost:9292/battlegrounds/new', config)
         .then(resp => resp.json())
         .then(resp => handleEncounterChange(resp))
+        .then(setPlayerAttacking(false))
+    }
+    const handleEncounterChange = (enc) => {
+        setBattlegrounds([...battlegrounds, enc])
+        setEnemies(enemies.map(enemy => enemy.id === enc.enemy.id? enemy = enc.enemy : enemy))
+    }
+    
+    const handleBattleReset = () => {
+        fetch('http://localhost:9292/battlegrounds/all', {method: "DELETE"})
+        .then(resp => resp.json())
+        .then(resp => {
+            setBattlegrounds(resp.battlegrounds)
+            setEnemies(resp.enemies)
+        })
     }
 
-    const listedSkirmishes = encounter.battlegrounds?.map(battle => {
+    const listedSkirmishes = battlegrounds?.map(battle => {
         return <p key={battle.id}>{`${battle.created_at}: ${battle.skirmish_log}! - ${battle.health_change} Damage`}</p>
     })
 
     return (
         <div className='gameboard'>
             <div className='battlefield'>
-                <EnemyHolder enemies={encounter.enemies} 
+                <EnemyHolder enemies={enemies} 
                             setTarget={setTarget} 
                             playerAttacking={playerAttacking}
                             onAttackSubmit={onAttackSubmit}
@@ -50,7 +77,7 @@ function Battleground({encounter, handleEncounterChange}) {
                 {listedSkirmishes}
             </div>
                                 
-                <CharacterHolder characters={encounter.characters} 
+                <CharacterHolder characters={characters} 
                                 setInitiator={setInitiator} 
                                 setPlayerAttacking={setPlayerAttacking} 
                                 playerAttacking={playerAttacking}
