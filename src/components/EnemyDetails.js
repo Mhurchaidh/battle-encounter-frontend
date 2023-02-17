0b91e93e1f274b29802329ac96738eeaddd37a6f
mhurchaidh <bmurchie1997@gmail.com>
2023-02-17 11:08:58 -0800

    Added attack confirmation cancel button

--- src/components/EnemyDetails.js
+++ src/components/EnemyDetails.js
@@ -13,12 +13,16 @@ function EnemyDetails({enemy, setTarget, playerAttacking, onAttackSubmit}) {
         setTargetSelected(false)
     }
 
+    const handleAttackCancel = () => {
+        setTargetSelected(false)
+    }
+
     return (
         <div className={`enemy-${enemy.enemy_class}`}>
             {enemy.name} - {enemy.enemy_class}
             <p>{enemy.mag_attack !== 0 ? `Mag: ${enemy.mag_attack}` : `Phys: ${enemy.phys_attack}`}</p>
             <p>{`HP: ${enemy.health}/${enemy.max_health}`}</p>
-            {playerAttacking? targetSelected? <button className='confirm' onClick={handleAttackConfirm}>Confirm</button> : <button onClick={handleAttack}>Select</button> : null}
+            {playerAttacking? targetSelected? <div><button className='confirm' onClick={handleAttackConfirm}>Confirm</button><button onClick={handleAttackCancel}>Cancel</button></div> : <button onClick={handleAttack}>Select</button> : null}
         </div>
     )
 }
